89732fe05ad3b6525f70a47c63288e8bc8af1c78
Thomas <tfk@debian>
2025-07-01 11:47:44 +0200

    add version number to sdk

---
+++ sdk/cmake_scripts/version.cmake
@@ -0,0 +1,33 @@
+include("${CMAKE_CURRENT_SOURCE_DIR}/../../cmake_scripts/nabto_version.cmake")
+cmake_policy(SET CMP0007 NEW)
+
+if ("${NABTO_WEBRTC_VERSION}" STREQUAL "")
+
+  nabto_version(version_out version_error)
+
+  if (NOT version_out)
+    if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/api/version.cpp)
+
+      message(FATAL_ERROR "No file ${CMAKE_CURRENT_SOURCE_DIR}/api/version.cpp exists and it cannot be auto generated. ${version_error}")
+    endif()
+  endif()
+else()
+  set(version_out ${NABTO_WEBRTC_VERSION})
+endif()
+
+if (version_out)
+  set(VERSION "#include <nabto/webrtc/device.hpp>\n
+#include <string>\n
+static const std::string version_str = \"${version_out}\"\n;
+const std::string nabto::webrtc::SignalingDevice::version() { return version_str; }\n")
+
+  if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/src/version.cpp)
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/src/version.cpp VERSION_)
+  else()
+    set(VERSION_ "")
+  endif()
+
+  if (NOT "${VERSION}" STREQUAL "${VERSION_}")
+    file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/src/version.cpp "${VERSION}")
+  endif()
+endif()
